6faef73f60fb5e9bdd11e2e47b22ae2cd69131bf
willy <willoid.webdev@gmail.com>
2025-04-11 18:13:03 +0200

    routing for submitAnswers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\QuizController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [QuizController::class, 'quiz'])->name('quiz');
+Route::get('/results', function () {
+    $score = session('score');
+    return view('results', ['score' => $score]);
+})->name('results');
 
-Route::get('/results', [QuizController::class, 'results'])->name('results');
+Route::post('/submit-answer', [QuizController::class, 'submitAnswer'])->name('submitAnswer');
