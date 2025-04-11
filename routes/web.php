067baef9c7f76fa6a074cb2f59e4e60a9a9a06b7
willy <willoid.webdev@gmail.com>
2025-04-11 17:46:34 +0200

    prepared frontend and routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [QuizController::class, 'quiz'])->name('quiz');
+
+Route::get('/results', [QuizController::class, 'results'])->name('results');
